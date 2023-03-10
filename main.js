06ee08b3d61abc0b0476eae505d3773f37726dcb
Haziev55 <127195241+Haziev55@users.noreply.github.com>
2023-03-10 22:27:26 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+document.querySelector('button').addEventListener('click', function(){
+document.querySelector('header').classList.toggle('open');
+})
+
+window.addEventListener("keydown", (e) => {
+if (e.key === "Escape") {
+document.querySelector("header").classList.remove("open");
+}
+});
+
+document.getElementById("menu").addEventListener("click", (event) => {
+  event._isClickWithInMenu = true;
+});
+document.getElementById("burger").addEventListener("click", (event) => {
+  event._isClickWithInMenu = true;
+});
+document.body.addEventListener("click", (event) => {
+  if (event._isClickWithInMenu) return;
+  document.querySelector("header").classList.remove("open");
+});
